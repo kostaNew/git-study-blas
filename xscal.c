65e17d814401f525894a9eefb3d069a211667241
Dmitry <berd_gow@mail.ru>
2015-04-18 01:51:06 +0500

    Multiplication on scalar

--- xscal.c
+++ xscal.c
@@ -3,8 +3,7 @@ void cblas_sscal (const int n, const float a, float *x, const int incx)
 	int i=0,t;
 	for(t=0;t<n;t++)
 	{
-		x[i] *= a;
-		i += incx;
+		*(x + incx * i) *= a;
 	}	
 }
 void cblas_dscal (const int n, const double a, double *x, const int incx)
@@ -12,7 +11,6 @@ void cblas_dscal (const int n, const double a, double *x, const int incx)
 	int i=0,t;
 	for(t=0;t<n;t++)
 		{
-		x[i] *= a;
-		i += incx;
+		*(x + incx * i) *= a;
 		}
 }
