397afa2b6cdffbf08672e6d96f000f355c4f1ffb
Dmitry <berd_gow@mail.ru>
2015-04-18 01:53:10 +0500

    Multiplication on scalar (workable)

--- xscal.c
+++ xscal.c
@@ -1,15 +1,15 @@
 void cblas_sscal (const int n, const float a, float *x, const int incx)
 {
-	int i=0,t;
-	for(t=0;t<n;t++)
+	int i=0;
+	for(i=0;i<n;i++)
 	{
 		*(x + incx * i) *= a;
 	}	
 }
 void cblas_dscal (const int n, const double a, double *x, const int incx)
 {
-	int i=0,t;
-	for(t=0;t<n;t++)
+	int i=0;
+	for(i=0;i<n;i++)
 		{
 		*(x + incx * i) *= a;
 		}
