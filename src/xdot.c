064f3191c3bdeac99e3e20e5c418eb2a01c73f00
kreolim <suspect91717@gmail.com>
2015-04-25 01:51:05 +0500

    Refactored xdot

--- src/xdot.c
+++ src/xdot.c
@@ -1,21 +1,15 @@
 float cblas_sdot (const int n, const float *x, const int incx, const float *y, const int incy) {
 	float result = 0;
-	int i;
-	for (i = 0; i < n; i++) {
-		float current_x = *(x + incx * i);
-		float current_y = *(y + incy * i);
-		result += current_x * current_y;
+	for (int i = 0; i < n; i++) {
+		result += x[i*incx] * y[i*incy];
 	}
 	return result;
 }
 
 double cblas_ddot (const int n, const double *x, const int incx, const double *y, const int incy) {
 	double result = 0;
-	int i;
-	for (i = 0; i < n; i++) {
-		double current_x = *(x + incx * i);
-		double current_y = *(y + incy * i);
-		result += current_x * current_y;
+	for (int i = 0; i < n; i++) {
+		result += x[i*incx] * y[i*incy];
 	}
 	return result;
 }
