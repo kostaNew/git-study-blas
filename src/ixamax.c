e767dad53c236b10bb16f8898262043842671102
The_uo <quillich775@gmail.com>
2015-04-24 19:49:20 +0500

    bugfix?

--- src/ixamax.c
+++ src/ixamax.c
@@ -1,8 +1,10 @@
 int cblas_isamax (const int n, const float *x, const int incx){
 	int ind = 0;
+	float elem;
 	float max = *(x + ind);
 	for(int i=0; i<n;i++){
-		if (*(x + incx*i)>max){
+		elem = *(x + incx*i);
+		if (elem >max){
 			ind = i;
 		}
 	}
@@ -11,8 +13,10 @@ return ind;
 int cblas_idamax (const int n, const double *x, const int incx){
 	int ind = 0;
 	double max = *(x + ind);
+	double elem;
 	for(int i=0; i<n;i++){
-		if (*(x + incx*i)>max){
+	    elem = *(x + incx*i);
+		if (elem >max){
 			ind = i;
 		}
 	}
