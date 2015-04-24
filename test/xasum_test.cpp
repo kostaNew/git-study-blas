ddf62d3ecb39b6a8861c15f2b5e394ebf7f53e0a
Genraim <egarmin95@gmail.com>
2015-04-24 19:37:13 +0500

    Create test for xasum

--- test/xasum_test.cpp
+++ test/xasum_test.cpp
@@ -3,6 +3,24 @@ extern "C" {
 	# include "xasum.h"
 }
 
-TEST(xasumTest, postive) {
-	SUCCEED();
+TEST(xasumFloatTest, postive) {
+	int size = 3;
+	float test[size];
+	for (int i = 0; i < size; i++) {
+		test[i] = i;
+
+	}
+	float result = cblas_sasum(size, test, 1);
+	ASSERT_FLOAT_EQ(3, result);
+}
+
+TEST(xasumDoubleTest, postive) {
+	int size = 3;
+	double test[size];
+	for (int i = 0; i < size; i++) {
+		test[i] = i;
+
+	}
+	double result = cblas_dasum(size, test, 1);
+	ASSERT_DOUBLE_EQ(3, result);
 }
