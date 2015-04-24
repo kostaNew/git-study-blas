48f6b34947aec2743c5b69e8004c8583c79c0fbd
kreolim <suspect91717@gmail.com>
2015-04-24 18:29:22 +0500

    Added xdot test

--- test/xdot_test.cpp
+++ test/xdot_test.cpp
@@ -3,6 +3,30 @@ extern "C" {
 	# include "xdot.h"
 }
 
-TEST(xdotTest, postive) {
-	SUCCEED();
+TEST(xdotFloatTest, postive) {
+	int size = 5;
+	float test1[size];
+	float test2[size];
+	for (int i = 0; i < size; i++) {
+		test1[i] = i;
+		test2[i] = 2*i;
+	}
+
+	float dot_res = cblas_sdot(size, test1, 1, test2, 1);
+
+	ASSERT_FLOAT_EQ(60, dot_res);
+} 
+
+TEST(xdotDoubleTest, postive) {
+	int size = 5;
+	double test1[size];
+	double test2[size];
+	for (int i = 0; i < size; i++) {
+		test1[i] = i;
+		test2[i] = 2*i;
+	}
+
+	double dot_res = cblas_ddot(size, test1, 1, test2, 1);
+	
+	ASSERT_DOUBLE_EQ(60, dot_res);
 } 
