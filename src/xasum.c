1ec3f164d18075af4f6814c23d0fffc18d95cc13
Konstantin Ushenin <kostaNew@gmail.com>
2015-04-25 18:52:34 +0500

    Code review. xasum.c

--- src/xasum.c
+++ src/xasum.c
@@ -1,6 +1,10 @@
 float cblas_sasum (const int n, const float *x, const int incx){
 	float result = 0;
 	for (int i = 0; i < n; i++) {
+		//Арифметика укаателей, это смерть производительности
+		//Старайтесь использовать [] везде где только можно
+		//Самым лучшим вариантом исправить такой код будет изменить условие прохода цикла на
+		//for (int i = 0; i < n*incx; i+=incx) и тело цикла под него
 		result += *(x + i * incx);
 	}
 	return result;
