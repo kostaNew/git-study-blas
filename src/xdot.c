a76f9c9a2ead61b4c67c23a7a0e6496bc3965e2e
Konstantin Ushenin <kostaNew@gmail.com>
2015-04-25 19:23:24 +0500

    Code review. OK :-)

--- src/xdot.c
+++ src/xdot.c
@@ -7,7 +7,7 @@ float cblas_sdot (const int n, const float *x, const int incx, const float *y, c
 }
 
 double cblas_ddot (const int n, const double *x, const int incx, const double *y, const int incy) {
-	double result = 0;
+	double result = 0; 
 	for (int i = 0; i < n; i++) {
 		result += x[i*incx] * y[i*incy];
 	}
