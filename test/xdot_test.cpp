270ca6adc6d4d2894fb5c5ea3859ac7a3043f919
kreolim <suspect91717@gmail.com>
2015-04-26 04:19:46 +0500

    Added increment tests for xdot

--- test/xdot_test.cpp
+++ test/xdot_test.cpp
@@ -3,32 +3,75 @@ extern "C" {
 	# include "xdot.h"
 }
 
-//Нужны тесты на inc? != 1
 
 TEST(xdotFloatTest, postive) {
-	int size = 5;
-	float test1[size];
-	float test2[size];
-	for (int i = 0; i < size; i++) {
-		test1[i] = i;
-		test2[i] = 2*i;
+	const int vector_size = 100;
+	const int inc = 1;
+	float x_vec[vector_size];
+	float y_vec[vector_size];
+	for (int i = 0; i < vector_size; i++) {
+		x_vec[i] = 1;
+		y_vec[i] = 2;
 	}
 
-	float dot_res = cblas_sdot(size, test1, 1, test2, 1);
+	float dot_res = cblas_sdot(vector_size, x_vec, inc, y_vec, inc);
 
-	ASSERT_FLOAT_EQ(60, dot_res);
-} 
+	ASSERT_FLOAT_EQ(200, dot_res);
+}
 
 TEST(xdotDoubleTest, postive) {
-	int size = 5;
-	double test1[size];
-	double test2[size];
-	for (int i = 0; i < size; i++) {
-		test1[i] = i;
-		test2[i] = 2*i;
+	const int vector_size = 100;
+	const int inc = 1;
+	double x_vec[vector_size];
+	double y_vec[vector_size];
+	for (int i = 0; i < vector_size; i++) {
+		x_vec[i] = 2;
+		y_vec[i] = 1;
 	}
 
-	double dot_res = cblas_ddot(size, test1, 1, test2, 1);
+	double dot_res = cblas_ddot(vector_size, x_vec, inc, y_vec, inc);
 	
-	ASSERT_DOUBLE_EQ(60, dot_res);
-} 
+	ASSERT_DOUBLE_EQ(200, dot_res);
+}
+
+TEST(xdotFloatIncTest, postive) {
+	const int vector_size = 150;
+	const int incx = 3;
+	const int incy = 5;
+	const int x_holder_size = 1+(vector_size-1)*incx;
+	const int y_holder_size = 1+(vector_size-1)*incy;
+	float x_vec[x_holder_size];
+	float y_vec[y_holder_size];
+	for (int i = 0; i < x_holder_size; i++) {
+		x_vec[i] = 1;
+		y_vec[i] = 2;
+	}
+	for (int i = x_holder_size; i < y_holder_size; i++) {
+		y_vec[i] = 2;
+	}
+
+	float dot_res = cblas_sdot(vector_size, x_vec, incx, y_vec, incy);
+	
+	ASSERT_FLOAT_EQ(300, dot_res);
+}
+
+TEST(xdotDoubleIncTest, postive) {
+	const int vector_size = 150;
+	const int incx = 5;
+	const int incy = 3;
+	const int x_holder_size = 1+(vector_size-1)*incx;
+	const int y_holder_size = 1+(vector_size-1)*incy;
+	double x_vec[x_holder_size];
+	double y_vec[y_holder_size];
+	for (int i = 0; i < y_holder_size; i++) {
+		x_vec[i] = 1;
+		y_vec[i] = 2;
+	}
+	for (int i = y_holder_size; i < x_holder_size; i++) {
+		x_vec[i] = 1;
+	}
+
+	double dot_res = cblas_ddot(vector_size, x_vec, incx, y_vec, incy);
+	
+	ASSERT_DOUBLE_EQ(300, dot_res);
+}
