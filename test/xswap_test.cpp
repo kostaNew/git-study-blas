421310931cbbf480c3ead19162483e89d0e77931
Konstantin Ushenin <kostaNew@gmail.com>
2015-04-25 19:36:12 +0500

    Code review

--- test/xswap_test.cpp
+++ test/xswap_test.cpp
@@ -3,6 +3,9 @@ extern "C" {
 	# include "xswap.h"
 }
 
+//Нужно два отдельных теста под double и float варианты.
+//Тесты при inc? != 1
+
 TEST(xswapTest, postive) {
     double a[10] = {1,2,3,4,5,6,7,8,9,10};
     double b[10] = {10,9,8,7,6,5,4,3,2,1};
