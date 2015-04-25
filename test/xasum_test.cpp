020448a3079c340c8fd925da0f949627366e6eef
Genraim <egarmin95@gmail.com>
2015-04-25 23:52:04 +0500

    Fix .gitignore, xasum and xasum_test

--- test/xasum_test.cpp
+++ test/xasum_test.cpp
@@ -7,23 +7,23 @@ extern "C" {
 //Нужны тесты на inc? != 1
 
 TEST(xasumFloatTest, postive) {
-	int size = 3;
+	const int size = 30;
 	float test[size];
 	for (int i = 0; i < size; i++) {
 		test[i] = i;
 
 	}
-	float result = cblas_sasum(size, test, 1);
-	ASSERT_FLOAT_EQ(3, result);
+	float result = cblas_sasum(size, test, 2);
+	ASSERT_FLOAT_EQ(210, result);
 }
 
 TEST(xasumDoubleTest, postive) {
-	int size = 3;
+	const int size = 30;
 	double test[size];
 	for (int i = 0; i < size; i++) {
 		test[i] = i;
 
 	}
-	double result = cblas_dasum(size, test, 1);
-	ASSERT_DOUBLE_EQ(3, result);
+	double result = cblas_dasum(size, test, 2);
+	ASSERT_DOUBLE_EQ(210, result);
 }
