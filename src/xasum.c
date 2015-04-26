2e1b97cc96cb586b38892e4e0dfa5e1497704fc8
Genraim <egarmin95@gmail.com>
2015-04-26 12:12:58 +0500

    Hotfix xasum

--- src/xasum.c
+++ src/xasum.c
@@ -1,19 +1,19 @@
 float cblas_sasum (const int n, const float *x, const int incx){
 	float result = 0;
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i+=incx) {
 		//Арифметика укаателей, это смерть производительности
 		//Старайтесь использовать [] везде где только можно
 		//Самым лучшим вариантом исправить такой код будет изменить условие прохода цикла на
 		//for (int i = 0; i < n*incx; i+=incx) и тело цикла под него
-		result += x[i*incx];
+		result += x[i];
 	}
 	return result;
 }
 
 double cblas_dasum (const int n, const double *x, const int incx){
 	double result = 0;
-	for (int i = 0; i < n; i++) {
-		result += x[i*incx];
+	for (int i = 0; i < n; i+=incx) {
+		result += x[i];
 	}
 	return result;
 }
--- test/xasum_test.cpp
+++ test/xasum_test.cpp
@@ -9,21 +9,23 @@ extern "C" {
 TEST(xasumFloatTest, postive) {
 	const int size = 30;
 	float test[size];
+	int incx = 2;
 	for (int i = 0; i < size; i++) {
 		test[i] = i;
 
 	}
-	float result = cblas_sasum(size, test, 1);
-	ASSERT_FLOAT_EQ(435, result);
+	float result = cblas_sasum(size, test, incx);
+	ASSERT_FLOAT_EQ(210, result);
 }
 
 TEST(xasumDoubleTest, postive) {
 	const int size = 30;
 	double test[size];
+	int incx = 2;
 	for (int i = 0; i < size; i++) {
 		test[i] = i;
 
 	}
-	double result = cblas_dasum(size, test, 1);
-	ASSERT_DOUBLE_EQ(435, result);
+	double result = cblas_dasum(size, test, incx);
+	ASSERT_DOUBLE_EQ(210, result);
 }
