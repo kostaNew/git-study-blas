c0bcefdd38f77f908fb0d59914de196a93e8eeda
konstantin <konstantin@kostanew-Rev-1-0>
2015-05-02 13:14:44 +0500

    Documentation

--- include/xswap.h
+++ include/xswap.h
@@ -1,8 +1,30 @@
 #ifndef XSWAP_H_
 #define XSWAP_H_
 
+/**
+    \file
+    \brief Меняет местами два массива.
+*/
+
+
+/**
+    Меняет местами массивы из элементов типа float
+    \param[in] n Размер массива
+    \param[in] x Ссылка на первый элемент первого массива
+    \param[in] incx Расстояние между элементами первого массива
+    \param[in] y Ссылка на первый элемент второго массива
+    \param[in] incy Расстояние между элементами второго массива
+ */
 void cblas_sswap (const int n, float *x, const int incx, float *y, const int incy);
 
+/**
+    Меняет местами массивы из элементов типа double
+    \param[in] n Размер массива
+    \param[in] x Ссылка на первый элемент первого массива
+    \param[in] incx Расстояние между элементами первого массива
+    \param[in] y Ссылка на первый элемент второго массива
+    \param[in] incy Расстояние между элементами второго массива
+ */
 void cblas_dswap (const int n, double *x, const int incx, double *y, const int incy);
 
 #endif // xswap.h
--- test/gtest_main.cpp
+++ test/gtest_main.cpp
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include "gtest.h"
 
+/**
+    \file
+    \brief Точка входа в программу и запуск тестов
+ 
+    Запуск консольного приложения всегда начинается с функции main.
+    Написанный в этой функции код инициализирует и запускает Google Test.
+    Компилируется компилятором C++, а не C. 
+    
+    (Это очень избыточное описание, но мне нужен пример для Doxygen)
+ 
+ */
 GTEST_API_ int main(int argc, char **argv) {
   printf("Running main() from gtest_main.cc\n");
   testing::InitGoogleTest(&argc, argv);
