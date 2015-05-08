b38e648f36afc9a535e85fc98a5c3a7f2654b314
kreolim <suspect91717@gmail.com>
2015-05-08 18:45:00 +0500

    Paralleled xdot functions

--- src/xdot.c
+++ src/xdot.c
@@ -1,6 +1,10 @@
+#include <omp.h>
+
 float cblas_sdot (const int n, const float *x, const int incx, const float *y, const int incy) {
 	float result = 0;
-	for (int i = 0; i < n; i++) {
+	#pragma omp parallel for reduction(+:result)
+	for (int i = 0; i < n; i++)
+	{
 		result += x[i*incx] * y[i*incy];
 	}
 	return result;
@@ -8,6 +12,7 @@ float cblas_sdot (const int n, const float *x, const int incx, const float *y, c
 
 double cblas_ddot (const int n, const double *x, const int incx, const double *y, const int incy) {
 	double result = 0; 
+	#pragma omp parallel for reduction(+:result)
 	for (int i = 0; i < n; i++) {
 		result += x[i*incx] * y[i*incy];
 	}
