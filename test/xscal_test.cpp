0bce42125ddcd089e36e46a25e12c893121d7f45
kreolim <suspect91717@gmail.com>
2015-05-09 00:53:56 +0500

    Fixed tests names. Please, compile before commiting

--- test/xscal_test.cpp
+++ test/xscal_test.cpp
@@ -11,7 +11,7 @@ extern "C" {
 /**
     Тест с массивом из 10 элементов с шагом=1 и скаляром=10.
  */
-TEST(xscalDoubleTest, postive) {
+TEST(xscalDoubleSimpleTest, postive) {
 		double a[10] = {1,2,3,4,5,6,7,8,9,10};
 		double b[10] = {10,20,30,40,50,60,70,80,90,100};
 		double alpha = 10;
@@ -25,7 +25,7 @@ TEST(xscalDoubleTest, postive) {
 /**
     Тест с умножением массива на 0.
  */
-TEST(xscalDoubleTest, postive) {
+TEST(xscalDoubleZeroTest, postive) {
 		double a[10] = {1,2,3,4,5,6,7,8,9,10};
 		double b[10] = {0,0,0,0,0,0,0,0,0,0};
 		double alpha = 0;
@@ -40,7 +40,7 @@ TEST(xscalDoubleTest, postive) {
 /**
     Тест с умножением массива на отрицательный скаляр.
  */
-TEST(xscalDoubleTest, postive) {
+TEST(xscalDoubleNegativeTest, postive) {
 		double a[10] = {1,-2,3,4,-5,6,7,8,-9,10};
 		double b[10] = {-3,6,-9,-12,15,-18,-21,-24,27,-30};
 		double alpha = -3;
